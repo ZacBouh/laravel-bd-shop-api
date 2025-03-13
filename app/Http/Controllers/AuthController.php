06e5524d859faa4d84c3a9cd2489ba504d5249d3
ZacBouh <zacharie.bouhaya@gmail.com>
2025-03-13 13:11:11 +0000

    feat: register and login with Sanctum statefulApi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Register successful',
+            'user' => $user
+        ]);
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required', 'string', 'max:255', 'email'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+
+
+        Log::info('Session:', [$request->session()]);
+        Log::info('Cookies:', [$request->cookies->all()]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => Auth::user()
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Invalid credentials'
+        ], status:401);
+    }
+}
